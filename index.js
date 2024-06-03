524174bdbc7e9cb061505a7e3c9b7037e697a827
Jujuzix <maronez.juliana@gmail.com>
2024-06-03 17:16:16 -0400

    aula de ptas 03.06

--- index.js
+++ index.js
@@ -1,29 +1,51 @@
-const express = require ("express");
-const exphbs = require ("express-handlebars");
+const express = require("express");
+const exphbs = require("express-handlebars");
 
 const app = express();
 
 app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
 
-app.get("/", (req, res)=>{
-    res.render("home");
+const usuarios = [
+    {
+        nome: "Jujuzona do Grau",
+        endereco: "Rua: Rua da Saudade 333",
+    },
+    {
+        nome: "Batatinha 123",
+        endereco: "casa do café",
+    },
+    {
+        nome: "Camz",
+        endereco: "Av.Ivinhema",
+    },
+];
+
+app.get("/", (req, res) => {
+    res.render("home", { usuarios });
 });
 
-app.get("/cadastro", (req, res)=>{
-    res.render("formCadastro")
+app.get("/cadastro", (req, res) => {
+    res.render("formCadastro",{usuarios})
 });
 
-app.get("/usuario/:id", (req, res)=>{
-    let nome = "Juliana";
-    res.render("usuario", {
-        nome, 
+app.put("/cadastro", (req, res)=>{
+    req.render(usuarios.push)
+})
+
+app.get("/usuario/:id", (req, res) => {
+    let usuario = {
+        nome: "Jujuzona do Grau",
+        endereco: "Rua: Rua da Saudade 333",
+    };
+    res.render("usuarios", {
+        usuario,
         id: req.params.id,
     });
 });
 
 
-app.listen(8000, ()=> {
+app.listen(8000, () => {
     console.log("Servidor está rodando corretamente!")
     console.log("http://localhost:8000/")
 })
